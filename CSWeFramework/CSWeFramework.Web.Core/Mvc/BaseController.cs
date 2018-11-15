8b935a68304528ab7352d69a19cb247f7c0dc6df
常帅 <550992499@qq.com>
2018-11-15 18:13:05 +0800

    Json自定义

---
+++ CSWeFramework/CSWeFramework.Web.Core/Mvc/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CSWeFramework.Web.Core.Mvc
+{
+    public class BaseController : Controller
+    {
+        protected override JsonResult Json(object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return Json(data, contentType, contentEncoding, behavior, null);
+        }
+
+        protected JsonResult Json(object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior, JsonSerializerSettings settings)
+        {
+            return new JsonNetResult { Data = data, ContentType = contentType, ContentEncoding = contentEncoding, JsonRequestBehavior = behavior, JsonSerializerSettings = settings };
+        }
+
+        protected virtual JsonResult Json(object data, JsonSerializerSettings jsonSerializerSettings)
+        {
+            return Json(data, null, null, JsonRequestBehavior.DenyGet, jsonSerializerSettings);
+        }
+    }
+}
---
+++ CSWeFramework/CSWeFramework.Web.Core/Mvc/JsonNetResult.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Web.Mvc;
+
+namespace CSWeFramework.Web.Core.Mvc
+{
+    public class JsonNetResult : JsonResult
+    {
+        public JsonSerializerSettings JsonSerializerSettings { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var response = context.HttpContext.Response;
+            response.ContentType = !string.IsNullOrEmpty(ContentType) ? ContentType : "application/json";
+
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+
+            JsonSerializerSettings jsonSerializerSettings = this.JsonSerializerSettings ?? new JsonSerializerSettings();
+            //忽略循环引用，不要序列化。如果设置 为Error，则遇到循环引用的时候报错
+            jsonSerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            jsonSerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();//json中属性开头字母小写的驼峰命名
+            jsonSerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            var json = JsonConvert.SerializeObject(Data, Formatting.None, jsonSerializerSettings);
+            response.Write(json);
+        }
+    }
+}
